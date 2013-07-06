14e8648c30321f3aa8e2543af307ec8883931eba
Brandon T. Fields <brandon@cdated.com>
2013-07-05 21:09:43 -0400

    Change the default shell to zsh in install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
+# Create all the symlinks
 ln -s vim ~/.vim
 ln -s vim/.vimrc ~/.vimrc
 ln -s .tmux.conf ~/.tmux.conf
 ln -s .zshrc ~/.zshrc
+
+# If zsh is install change it to the default shell
+ZSH_PATH=$(whereis zsh)
+if [ ! -z $ZSH_PATH ]; then
+  chsh -s $ZSH_PATH
+fi
