d3dae461844fb8146681e4cc231b015f0e1999ed
Brandon T. Fields <brandon@cdated.com>
2015-12-06 17:41:51 -0500

    Update dotfile install script

--- install.sh
+++ install.sh
@@ -1,19 +1,35 @@
 #!/bin/sh
 
-curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+# Install OMZ if not installed
+OMZ_PATH='~/.oh-my-zsh'
+if [ -z $OMZ_PATH ]; then
+  curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+  ln -s ${PWD}/zsh/cdated.zsh-theme ~/.oh-my-zsh/themes
+fi
 
-# Removing OMZ .zshrc
-rm ~/.zshrc
+echo "OMZ installed"
 
 # Create all the symlinks
+rm ~/.vim
 ln -s ${PWD}/vim ~/.vim
-ln -s ${PWD}/vim/.vimrc ~/.vimrc
-ln -s ${PWD}/.tmux.conf ~/.tmux.conf
-ln -s ${PWD}/.zshrc ~/.zshrc
-ln -s ${PWD}/zsh/cdated.zsh-theme ~/.oh-my-zsh/themes
 
-# If zsh is install change it to the default shell
-ZSH_PATH=$(whereis zsh)
-if [ ! -z $ZSH_PATH ]; then
+rm ~/.vimrc
+ln -s ${PWD}/vim/.vimrc ~/.vimrc
+
+rm ~/.tmux.conf
+ln -s ${PWD}/.tmux.conf ~/.tmux.conf
+
+rm ~/.zshrc
+ln -s ${PWD}/.zshrc ~/.zshrc
+
+echo "Symlinked updated"
+
+# If zsh is installed change default shell to zsh
+ZSH_PATH=/bin/zsh
+if [ -x $ZSH_PATH ] && [ $SHELL != $ZSH_PATH ]; then
   chsh -s $ZSH_PATH
 fi
+
+echo "Current shell set to zsh"
+
+echo "Done!"
