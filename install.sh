10c4f18b0544e5f9b7dd6fdf10f0e8cd1f21d0d5
Brandon T. Fields <brandon@cdated.com>
2015-12-06 22:25:01 -0500

    Ensure omz theme gets replace during install

--- install.sh
+++ install.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
 # Install OMZ if not installed
-OMZ_PATH='~/.oh-my-zsh'
+OMZ_PATH=~/.oh-my-zsh
 if [ -z $OMZ_PATH ]; then
-  curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
-  ln -s ${PWD}/zsh/cdated.zsh-theme ~/.oh-my-zsh/themes
+    curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
 fi
+
+# Replace cdated omz theme
+if [ ! -z $OMZ_PATH ]; then
+    rm $OMZ_PATH/themes/cdated.zsh-theme
+fi
+ln -s ${PWD}/zsh/cdated.zsh-theme $OMZ_PATH/themes/cdated.zsh-theme
 
 echo "OMZ installed"
 
@@ -27,7 +32,7 @@ echo "Symlinked updated"
 # If zsh is installed change default shell to zsh
 ZSH_PATH=/bin/zsh
 if [ -x $ZSH_PATH ] && [ $SHELL != $ZSH_PATH ]; then
-  chsh -s $ZSH_PATH
+    chsh -s $ZSH_PATH
 fi
 
 echo "Current shell set to zsh"
