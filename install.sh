3f5faf52e7fa8d520a2579c4999024996633cd0b
Brandon T. Fields <brandon@cdated.com>
2015-12-14 23:23:09 -0500

    Update dot files install script to be more resilent to the install env

--- install.sh
+++ install.sh
@@ -2,39 +2,46 @@
 
 # Install OMZ if not installed
 OMZ_PATH=~/.oh-my-zsh
-if [ -z $OMZ_PATH ]; then
+ZSH_THEME=$OMZ_PATH/themes/cdated.zsh-theme
+
+# Install OMZ if not found
+if [[ ! -d $OMZ_PATH ]]; then
     curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+    echo "OMZ installed"
 fi
-
-# Replace cdated omz theme
-if [ ! -z $OMZ_PATH ]; then
-    rm $OMZ_PATH/themes/cdated.zsh-theme
-fi
-ln -s ${PWD}/zsh/cdated.zsh-theme $OMZ_PATH/themes/cdated.zsh-theme
-
-echo "OMZ installed"
-
-# Create all the symlinks
-rm ~/.vim
-ln -s ${PWD}/vim ~/.vim
-
-rm ~/.vimrc
-ln -s ${PWD}/vim/.vimrc ~/.vimrc
-
-rm ~/.tmux.conf
-ln -s ${PWD}/.tmux.conf ~/.tmux.conf
-
-rm ~/.zshrc
-ln -s ${PWD}/.zshrc ~/.zshrc
-
-echo "Symlinked updated"
 
 # If zsh is installed change default shell to zsh
 ZSH_PATH=/bin/zsh
 if [ -x $ZSH_PATH ] && [ $SHELL != $ZSH_PATH ]; then
     chsh -s $ZSH_PATH
 fi
-
 echo "Current shell set to zsh"
+
+# Replace theme with repo file
+if [[ -a $ZSH_THEME ]]; then
+    rm $ZSH_THEME
+fi
+ln -s ${PWD}/zsh/cdated.zsh-theme $ZSH_THEME
+echo "cdated theme installed"
+
+if [[ -d ~/.vim ]]; then
+    rm -rf ~/.vim
+fi
+ln -s ${PWD}/vim ~/.vim
+echo ".vim directory linked"
+
+# Create all the symlinks
+for file in .vimrc .tmux.conf .zshrc
+do
+    if [[ -h ~/${file} ]] || [[ -a ~/${file} ]]; then
+        rm ~/$file
+    fi
+    ln -s ${PWD}/$file ~/$file
+done
+echo "rc files linked"
+
+cd vim
+git submodule init
+git submodule update
 
 echo "Done!"
