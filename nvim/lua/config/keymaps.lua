6ea2e3c5ee1e51b804acf2baad49aacdc93f1c37
Brandon T. Fields <brandon@cdated.com>
2024-04-22 23:07:53 -0400

    Add neovim configs

---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,6 @@
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Add any additional keymaps here
+
+vim.keymap.del({ "n", "i", "v" }, "<A-j>")
+vim.keymap.del({ "n", "i", "v" }, "<A-k>")
---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,11 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+local opt = vim.opt
+
+opt.relativenumber = false -- Relative line numbers
+opt.mouse = ""
+vim.opt.statuscolumn = ""
+vim.opt.signcolumn = "number"
+vim.cmd([[hi SignColumn guibg=#151515]])
