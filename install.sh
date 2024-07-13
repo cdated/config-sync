173bbf5830f6b3af862c0ccd802efa720767f667
Brandon T. Fields <brandon@cdated.com>
2024-07-13 15:32:28 -0400

    chore: format installer and install zsh theme in custom

--- install.sh
+++ install.sh
@@ -3,55 +3,56 @@
 # If zsh is installed change default shell to zsh
 ZSH_PATH=$(command -v zsh)
 if [ ! $? -eq 0 ] || [ ! -e $ZSH_PATH ]; then
-	echo "Please install zsh first"
-	exit 1
+    echo "Please install zsh first"
+    exit 1
 fi
 
 if [ -x $ZSH_PATH ] && [ $SHELL != $ZSH_PATH ]; then
-	chsh -s $ZSH_PATH
+    chsh -s $ZSH_PATH
 fi
 
 echo "Current shell set to zsh"
 
 # Install OMZ if not installed
 OMZ_PATH=~/.oh-my-zsh
-ZSH_THEME=$OMZ_PATH/themes/cdated.zsh-theme
+ZSH_THEME=$OMZ_PATH/custom/themes/cdated.zsh-theme
 
 # Install OMZ if not found
 if [ ! -d $OMZ_PATH ]; then
-	curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
-	git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
-	echo "OMZ installed"
+    curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+    git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
+    echo "OMZ installed"
 fi
 
 # Replace theme with repo file
 if [ -e $ZSH_THEME ]; then
-	rm $ZSH_THEME
+    rm $ZSH_THEME
 fi
+mkdir -p $OMZ_PATH/custom/themes
 ln -s ${PWD}/zsh/cdated.zsh-theme $ZSH_THEME
 echo "cdated theme installed"
 
 if [ -d ~/.vim ]; then
-	rm -rf ~/.vim
+    rm -rf ~/.vim
 fi
 ln -s ${PWD}/vim ~/.vim
 echo ".vim directory linked"
 
 if [ -d ~/.config/nvim ]; then
-	rm -rf ~/.config/nvim
+    rm -rf ~/.config/nvim
 fi
 ln -s ${PWD}/nvim ~/.config/nvim
 echo ".nvim directory linked"
 
 if [ -d ~/.i3 ]; then
-	rm -rf ~/.i3
+    rm -rf ~/.i3
 fi
 ln -s ${PWD}/i3 ~/.i3
 echo ".i3 directory linked"
 
 if [ ! -f ~/.gitconfig ]; then
-	cp ${PWD}/.gitconfig ~/.gitconfig
-	echo ".gitconfig linked"
+    cp ${PWD}/.gitconfig ~/.gitconfig
+    echo ".gitconfig linked"
 fi
 
 cp ${PWD}/.Xmodmap ~/
@@ -59,10 +60,10 @@ echo "copied xmodmap for caps->ctrl"
 
 # Create all the symlinks
 for file in .bashrc .vimrc .tmux.conf .zshrc .taskrc; do
-	if [ -h ~/${file} ] || [ -e ~/${file} ]; then
-		rm ~/$file
-	fi
-	ln -s ${PWD}/$file ~/$file
+    if [ -h ~/${file} ] || [ -e ~/${file} ]; then
+        rm ~/$file
+    fi
+    ln -s ${PWD}/$file ~/$file
 done
 echo "rc files linked"
 
